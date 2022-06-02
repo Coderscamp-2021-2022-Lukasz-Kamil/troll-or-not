629f0868b068afeee43781bb540540c6b3fc9e51
Your Name <donata1989@gmail.com>
2022-06-02 09:45:48 +0200

    Fix theme and some styles

--- client/src/global-styles/GlobalStyles.tsx
+++ client/src/global-styles/GlobalStyles.tsx
@@ -1,11 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-*, *:before, *:after{
+   *, *:before, *:after{
    box-sizing: border-box;
    padding: 0;
    margin: 0;
-}
+   }
+
+   body {
+      font-family: 'Kaushan Script', cursive;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+
+      input::-webkit-outer-spin-button,
+      input::-webkit-inner-spin-button {
+         -webkit-appearance: none;
+         margin: 0;
+      }
+      input[type=number]{
+         -moz-appearance: textfield;
+      }
+
+   }
 `;
 
 export default GlobalStyles;
