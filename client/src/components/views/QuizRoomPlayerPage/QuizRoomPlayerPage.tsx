9c9c4f5f1c51b16163ffe082f5c0ad122c9115d8
Asia Czyżewska <jczyzewska96@gmail.com>
2022-06-04 22:54:38 +0200

    add question logic

--- client/src/components/views/QuizRoomPlayerPage/QuizRoomPlayerPage.tsx
+++ client/src/components/views/QuizRoomPlayerPage/QuizRoomPlayerPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Button } from "../../ui/Button/Button.style";
 import { TitlePic } from "../../ui/title/Title";
@@ -40,42 +40,45 @@ export const Video = styled.div<VideoProps>`
 
 
 const QuizRoomPlayerPage = () => {
+    const [isCurrentPlayer, setCurrentPlayer] = useState(false);
     const answers = ["Answer 1", "Answer 2", "Answer 3", "Answer 4"]; 
     const observers = [{name: "Kamil", ID: "222333"}, {name: "Donata", ID: "222333"}, {name: "Rafał", ID: "222333"}];
     const player = {name: "Mateusz", chatID: "222333"};
 
     return (
-    <FlexWrapper justifyContent="center">
-        <GameViewContainer>
-            <FlexWrapper direction="column" align-items="center">
-                <TitlePic marginBottom="0px"/>
-                {/* <Title></Title> */}
-                <Video width={666} height={400}>VideoComponent</Video>
-                <Typography fontSize="mds">{player.name}</Typography>
-                <FlexWrapper direction="column">
-                    <Typography fontSize="lg">Pytanie</Typography>
-                    <AnswersContainer>
-                        {answers.map((answer) => (
-                            <Button background="answer" width={300} height={77} fontSize="lg">{answer}</Button>
-                        ))}
-                    </AnswersContainer>
-                    <FlexWrapper justifyContent="space-between">
-                        <Button background="true" hoverBackground="trueHover" width={300} height={70} fontSize="lg">Prawda</Button>
-                        <Button background="false" hoverBackground="falseHover" width={300} height={70} fontSize="lg">Fałsz</Button>
+    <>
+        <FlexWrapper justifyContent="center">
+            <GameViewContainer>
+                <FlexWrapper direction="column" align-items="center">
+                    <TitlePic marginBottom="0px"/>
+                    {isCurrentPlayer ? "" :
+                    <Video width={666} height={400}>VideoComponent</Video>}
+                    <Typography fontSize="mds">{player.name}</Typography>
+                    <FlexWrapper direction="column">
+                        <Typography fontSize="lg">Pytanie</Typography>
+                        <AnswersContainer>
+                            {answers.map((answer) => (
+                                <Button background="answer" width={300} height={77} fontSize="lg">{answer}</Button>
+                            ))}
+                        </AnswersContainer>
+                        <FlexWrapper justifyContent="space-between">
+                            <Button background="true" hoverBackground="trueHover" width={300} height={70} fontSize="lg">Prawda</Button>
+                            <Button background="false" hoverBackground="falseHover" width={300} height={70} fontSize="lg">Fałsz</Button>
+                        </FlexWrapper>
                     </FlexWrapper>
                 </FlexWrapper>
-            </FlexWrapper>
-            <FlexWrapper direction="column" justifyContent="flex-start">
-                {observers.map((observer) => (
-                    <>
-                    <Video width={244} height={244}>{observer.ID}</Video>
-                    <Typography fontSize="mds">{observer.name}</Typography>
-                    </>)
-                )}
-                <div>Timer</div> 
-            </FlexWrapper>
-        </GameViewContainer>
-    </FlexWrapper>
+                <FlexWrapper direction="column" justifyContent="flex-start">
+                    {observers.map((observer) => (
+                        <>
+                        <Video width={244} height={244}>{observer.ID}</Video>
+                        <Typography fontSize="mds">{observer.name}</Typography>
+                        </>)
+                    )}
+                    <div>Timer</div> 
+                </FlexWrapper>
+            </GameViewContainer>
+        </FlexWrapper>
+    </>
     )
 }
 
