2e4fceb34da76b82102fd8a0ee600d64c4158e4f
Anna Pryma <rafal.pryma32@gmail.com>
2022-06-05 09:52:37 +0200

    display form

--- client/src/components/views/GameFinished/GameFisnished.tsx
+++ client/src/components/views/GameFinished/GameFisnished.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { CurrentPoints } from '../../../services/games/types';
 import { getUserNickName } from '../../../services/user/getUserNickName';
+import Modal from "../../ui/Modal/Modal"
 
 const GameFinished = ({currentPoints}: {currentPoints: CurrentPoints[]}) => {
-
+    const [modalOpen, setModalOpen] = useState(false);
     const [points, setPoints] = useState<{nick: string, points: number}[]>([]);
 
     useEffect(() => {
@@ -21,7 +22,10 @@ const GameFinished = ({currentPoints}: {currentPoints: CurrentPoints[]}) => {
     }, [])
 
     console.log(points);
-    return <div>Gra kurwa skończona</div>
+    return <div>
+       
+            { <Modal setOpenModal={setModalOpen} />} 
+    </div>
 }
 
 export default GameFinished;
