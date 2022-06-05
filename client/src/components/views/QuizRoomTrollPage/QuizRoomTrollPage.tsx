5fb2672eba46649b9cac77249e9bfebe6042ae1b
Anna Pryma <rafal.pryma32@gmail.com>
2022-06-05 07:26:41 +0200

    create modal

--- client/src/components/views/QuizRoomTrollPage/QuizRoomTrollPage.tsx
+++ client/src/components/views/QuizRoomTrollPage/QuizRoomTrollPage.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "./Form";
+import Modal from "../../ui/Modal/Modal"
+
 const QuizRoomTrollPage = () => {
+    const [modalOpen, setModalOpen] = useState(false);
     return (
-        <div>QuizRoomTrollPage</div>
+        <div>QuizRoomTrollPage
+
+          <button
+        
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Open
+      </button>
+       {modalOpen && <Modal setOpenModal={setModalOpen} />}
+        </div>
     )
 }
 
